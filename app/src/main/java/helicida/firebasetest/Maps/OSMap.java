7cad95d4a17af4552991fcd6f52e01caf5a87633
Sergi Barjola <sergibca@gmail.com>
2016-02-12 15:38:41 +0100

    Implementado el mapa con los marcadores en clustes, localizacion del usuario, etc.

--- app/src/main/java/helicida/firebasetest/Maps/OSMap.java
+++ app/src/main/java/helicida/firebasetest/Maps/OSMap.java
@@ -1,23 +1,37 @@
 package helicida.firebasetest.Maps;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
 import org.osmdroid.api.IMapController;
 import org.osmdroid.bonuspack.clustering.RadiusMarkerClusterer;
+import org.osmdroid.bonuspack.overlays.Marker;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.MinimapOverlay;
 import org.osmdroid.views.overlay.ScaleBarOverlay;
 import org.osmdroid.views.overlay.compass.CompassOverlay;
+import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
+import helicida.firebasetest.Mensaje;
 import helicida.firebasetest.R;
 
 /**
@@ -33,12 +47,18 @@ import helicida.firebasetest.R;
  */
 public class OSMap extends Fragment {
 
-    private MapView map;
-    private IMapController mapController;
+    // Mapa
+    private MapView map;                              // Mapa
+    private IMapController mapController;             // Controlador del mapa
 
+    // Elementos visuales del mapa
+    private CompassOverlay brujulaOverlay;            // Brujula
+    private ScaleBarOverlay escalaOverlay;            // Barra que indica la escala del mapa
+    private MyLocationNewOverlay miPosicionOverlay;   // Marcador de donde nos encontramos
+    private RadiusMarkerClusterer marcadoresMensajes; // Cluster de los marcadores de los mensajes
 
     public OSMap() {
-        // Required empty public constructor
+        // es necesario tener un constructor vacio
     }
 
     @Override
@@ -49,14 +69,89 @@ public class OSMap extends Fragment {
         // Declaramos el mapa
         map = (MapView) view.findViewById(R.id.map);
 
-        // Ajustamos el mapa con los controles y el zoom deseado
+        // Ajustamos el mapa con los controles, los elementos a mostrar y el zoom deseado
         ajustarMapa();
+
+        // Ponemos los marcadores
+        marcadoresMapa();
+
         map.invalidate();
 
         return view;
     }
 
+    private void marcadoresMapa(){
+
+        // Declaramos un overlay con nuestra posicion
+        miPosicionOverlay = new MyLocationNewOverlay(
+                getContext(),
+                new GpsMyLocationProvider(getContext()),
+                map
+        );
+
+        map.getOverlays().add(miPosicionOverlay);   // Situamos el marcador de nuestra posicion en el mapa
+
+        miPosicionOverlay.enableMyLocation();   // Activamos la localizacion
+
+        // Cuando se abra el mapa que nos lleve a nuestra localizacion
+        miPosicionOverlay.runOnFirstFix(new Runnable() {
+            public void run() {
+                mapController.animateTo( miPosicionOverlay.getMyLocation());
+            }
+        });
+
+        // Rutas y nodos firebase
+        Firebase ref =  new Firebase("https://helicidatest.firebaseio.com/");
+        final Firebase mensajes = ref.child("mensaje");
+
+        // Creamos un cluster (es decir, la acumulación de mensajes en un mismo marcador)
+        marcadoresMensajes = new RadiusMarkerClusterer(getContext());
+        map.getOverlays().add(marcadoresMensajes);
+
+        // Le damos la imagen drawable que queremos que tenga
+        Drawable clusterIconD = getResources().getDrawable(R.drawable.message_cluster);
+        Bitmap clusterIcon = ((BitmapDrawable)clusterIconD).getBitmap();
+
+        // Y le definimos nuestra imagen y ajustamos el tamaño
+        marcadoresMensajes.setIcon(clusterIcon);
+        marcadoresMensajes.setRadius(100);
+
+        mensajes.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                for (DataSnapshot postSnapshot : snapshot.getChildren()) {
+
+                    // Mensaje que extraemos de Firebase
+                    Mensaje mensaje = postSnapshot.getValue(Mensaje.class);
+
+                    // Definimos el marcador y hacemos que nos marque la localización del mensaje
+                    Marker marker = new Marker(map);
+                    GeoPoint point = new GeoPoint(mensaje.getLatitud(), mensaje.getLongitud());
+                    marker.setPosition(point);
+
+                    // Ajustamos el tamaño, la transparencia
+                    marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+                    marker.setAlpha(0.6f);
+
+                    // Le ponemos el título y la descripción
+                    marker.setTitle(mensaje.getTitulo());
+                    marker.setSubDescription(mensaje.getDescripcion());
+
+                    marcadoresMensajes.add(marker);
+                }
+                marcadoresMensajes.invalidate();
+                map.invalidate();
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                System.out.println("The read failed: " + firebaseError.getMessage());
+            }
+        });
+    }
+
     private void ajustarMapa() {
+
         // Ajustamos los valores
         map.setTileSource(TileSourceFactory.MAPQUESTOSM);
         map.setTilesScaledToDpi(true);
@@ -65,11 +160,31 @@ public class OSMap extends Fragment {
         map.setBuiltInZoomControls(true);
         map.setMultiTouchControls(true);
 
-        /* Movemos el mapa a la posición que queremos IESPoblenou
-        GeoPoint gPt = new GeoPoint(41.39825598, 2.20325277);
-        mapController.setCenter(gPt);*/
+        // Declaramos una escala del mapa
+        escalaOverlay = new ScaleBarOverlay(map);
 
-        setZoom(9);
+        // Lo situamos en el centro del mapa
+        escalaOverlay.setCentred(true);
+
+        // Hacemos calculos para que tome la dimension de pixeles correcta
+        final DisplayMetrics dm = getResources().getDisplayMetrics();
+        escalaOverlay.setScaleBarOffset(dm.widthPixels / 2, 10);
+
+        // Declaramos la brujula
+        brujulaOverlay = new CompassOverlay(
+                getContext(),
+                new InternalCompassOrientationProvider(getContext()),
+                map
+        );
+
+        brujulaOverlay.enableCompass();   // Activamos la brujula
+
+        // Mostramos los elementos en el mapa
+        map.getOverlays().add(this.escalaOverlay);      // Mostramos la barra con la escala
+        map.getOverlays().add(this.brujulaOverlay);     // Mostramos la brujula
+
+        // Le damos el zoom deseado
+        setZoom(12);
     }
 
     private void setZoom(int zoom) {
